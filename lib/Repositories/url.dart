5966bc7a4a2dd7193bccd6af371705fb8a2df98a
KevinEs14 <escobarkev@gmail.com>
2021-11-04 19:35:43 -0400

    correction of time currency in the music player and merge with Luis

--- lib/Repositories/url.dart
+++ lib/Repositories/url.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 // var directionUrl="http://192.168.1.11:8080/";
-var directionUrl="http://192.168.42.99:8080/";
-//var directionUrl="http://192.168.1.7:8080/";
+// var directionUrl="http://192.168.42.99:8080/";
+var directionUrl="http://192.168.1.7:8080/";
